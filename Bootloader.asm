0bec7ba50738484b1b2c75f09ba267d03cd5de6e
Wasgyst <ohsatanalimbrb@gmail.com>
2026-04-20 22:19:21 +0500

    Update Bootloader.asm

--- Bootloader.asm
+++ Bootloader.asm
@@ -1,13 +1,13 @@
 jl 0x2
-mov edx,0xddaadeda
+mov edx, 0xddaadeda
 jmp 0xaa1:0xfceebe0e
-adc dh,cl
-lods eax,DWORD PTR ds:[esi]
-fdivp st(6),st
-in eax,0x10
+adc dh, cl
+lods eax, DWORD PTR ds:[esi]
+fdivp st(6), st
+in eax, 0x10
 fisttp DWORD PTR [edx]
 aad 0x10
-mov esi,0xae5ba5da
+mov esi, 0xae5ba5da
 .byte 0xbe
 push ecx
 .byte 0x15
