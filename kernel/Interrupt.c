03801f2a060410ba42e0e9a4f412bdc072771387
justindude057-collab <justindude057@gmail.com>
2026-04-29 23:10:09 -0400

    Refactor main function and syscall implementation

--- kernel/Interrupt.c
+++ kernel/Interrupt.c
@@ -1,8 +1,7 @@
+int syscall() {
+ // im too lazy ti implement interupt here.
+return 0;}
+
 int main() {
 syscall();
 return 0;}
-
-int syscall() {
-_asm_ (""); // im too lazy ti implement interupt here.
-return 0;}
-
