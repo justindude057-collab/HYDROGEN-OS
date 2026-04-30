d3c62fe9d5f6b13af927ad0f4a6d06f2c666037d
justindude057-collab <justindude057@gmail.com>
2026-04-30 12:19:01 -0400

    Refactor memcall.c to define memcall function

--- kernel/memcall.c
+++ kernel/memcall.c
@@ -1,11 +1,5 @@
-//memoryprovider
-int ERROROFALLOW() {
-//if mem allocation fails
-return 0;}
-int processmemallow() {
-// mem allocation for process
-return 0;}
-//start
-int main(){
-processmemallow();
-return 0;}
+#ifndef memcall_H  
+#define memcall_H
+void memcall(){
+}
+#endif                
