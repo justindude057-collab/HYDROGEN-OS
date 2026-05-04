18e1ea63a35f467f5c49e0e17c3bf4ae45564ad1
justindude057-collab <justindude057@gmail.com>
2026-05-03 22:09:43 -0400

    Fix and refactor kernel function implementations
    Refactor kernel functions and fix syntax errors.

--- kernel/Krnlcall.c
+++ kernel/Krnlcall.c
@@ -1,22 +1,24 @@
 #include "intterupt.h"
 #include "memcall.h"
 #include "disk.h"
-
+int main(){
+return 0;}
 #ifndef Krnlcall_H  
 #define Krnlcall_H
+
 int memcall;
-memcall = 0;
+void memcall(){memcall = 0;
 if (memcall == 1){
-int i = 1}
+int i = 1}}
 
 int intterupt;
-intterupt = 0;
+void intterupt(){intterupt = 0;
 if (intterupt == 1){
-int A = 1}
+int A = 1}}
 
 int diskacess;
-diskacess = 0;
+void disk(){diskacess = 0;
 if (diskacess == 1){
-int disk = 1}
+int disk = 1}}
 
 #endif
