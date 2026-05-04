77fbe8bf757d695b8a2af543a64b163a621b1a85
justindude057-collab <justindude057@gmail.com>
2026-05-04 13:48:54 -0400

    Refactor bootloader in BOOTSECTION.asm
    Refactor bootloader code to improve readability and functionality.

--- BOOTSECTION.asm
+++ BOOTSECTION.asm
@@ -1,7 +1,19 @@
 org 0x7C00
 ;16 bit 
-start:
+
 ;i hate evey fucking bit of this asm 
+
+    mov si, title    
+    mov ah, 0x0e       
+
+start:
+    lodsb              
+    or al, al       
+    jz halt            
+    int 0x10           
 Jmp start
+
+title db "Kernaline os bootloader.", 0 
+
 Times 510 db 0
 db 0x55, 0xaa 
