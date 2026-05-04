a5e871c183d10fceb9fcc2e818a676db01883754
justindude057-collab <justindude057@gmail.com>
2026-05-04 13:55:36 -0400

    Update BOOTSECTION.asm

--- BOOTSECTION.asm
+++ BOOTSECTION.asm
@@ -18,5 +18,21 @@ hlt
 
 title db "Kernaline os bootloader.", 0 
 
+asm 
+
+    mov si, select    
+    mov ah, 0x0e       
+
+A:
+    lodsb              
+    or al, al       
+    jz halt            
+    int 0x10           
+Jmp A
+
+
+select db "select a drive..", 0 
+
+
 Times 510 db 0
 db 0x55, 0xaa 
